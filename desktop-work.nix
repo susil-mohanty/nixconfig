6f661702c98fbfff7b559356b4407e3cb88ade4f
Mathis Antony <sveitser@gmail.com>
2018-08-28 11:56:03 +0800

    Use shared config for desktop at work

--- desktop-work.nix
+++ desktop-work.nix
@@ -12,24 +12,9 @@
 
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+     ./hardware-configuration.nix
+     ./pc-common.nix
     ];
-
-  nix.buildCores = 0;
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    version = 2;
-    efiSupport = true;
-    enableCryptodisk = true;
-    extraInitrd = /boot/initrd.keys.gz;
-  };
 
   boot.initrd.luks.devices = [
       {
@@ -46,138 +31,11 @@
   boot.extraModulePackages = [ pkgs.linuxPackages.rtl8821au ];
 
   networking.hostName = "mas";
-  # networking.wireless.enable = true;
   networking.networkmanager.enable = true;
-  # networking.firewall.allowedTCPPorts = [ 5901 ];
 
-  # powerManagement.enable = false;
-  powerManagement.cpuFreqGovernor = "performance";
+  # environment.systemPackages = with pkgs; [
+  #   linuxPackages.rtlwifi_new
+  # ];
 
-  i18n = {
-     consoleKeyMap = "colemak/en-latin9";
-     defaultLocale = "en_US.UTF-8";
-  };
-
-  time.timeZone = "Asia/Hong_Kong";
-
-  environment.systemPackages = with pkgs; [
-    # nmon # not available
-    alsaUtils
-    usbutils
-    ag
-    curl
-    direnv
-    firefox
-    # fish
-    fzf
-    git
-    htop
-    pass
-    tree
-    plasma-nm
-    vim
-    wget
-    gnupg
-    wmname
-    # rtlwifi_new
-    linuxPackages.rtlwifi_new
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  programs.bash.enableCompletion = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-  programs.slock.enable = true;
-
-  programs.fish.enable = true;
-
-  services.openssh.enable = true;
-  services.locate.enable = true;
-  services.pcscd.enable = true;
-  services.compton.enable = true;
-  services.resolved.enable = true;
-
-  # Gitlab CI
-  # services.gitlab-runner.enable = true;
-  # services.gitlab-runner.configFile = /home/lulu/.gitlab-runner/config.toml;
-
-  fonts.fonts = with pkgs; [
-    fira-code
-    iosevka
-    noto-fonts
-    noto-fonts-cjk
-  ];
-
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.daemon.config = {
-    flat-volumes = "no";
-    resample-method = "src-sinc-best-quality";
-    default-sample-format = "s24le";
-    default-sample-rate = "96000";
-  };
-
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.gutenprint ];
-
-  hardware.trackpoint.enable = true;
-
-  services.xserver = {
-     synaptics.enable = true;
-     enable = true;
-     xkbVariant = "colemak";
-
-     displayManager.sddm.autoLogin.enable = true;
-     displayManager.sddm.autoLogin.user = "lulu";
-     displayManager.sddm.enable = true;
-
-     desktopManager.xterm.enable = false;
-     # desktopManager.plasma5.enable = true;
-
-     windowManager.xmonad = {
-       enable = true;
-       enableContribAndExtras = true;
-       extraPackages = haskellPackages: [
-         haskellPackages.xmonad-contrib
-         haskellPackages.xmonad-extras
-         # haskellPackages.xmonad
-       ];
-     };
-     windowManager.default = "xmonad";
-
-     # windowManager.exwm = {
-     #   enable = true;
-     # };
-     # windowManager.default = "exwm";
-  };
-
-  security.pam.loginLimits = [
-  {
-    domain = "*";
-    type = "soft";
-    item = "nofile";
-    value = "100000";
-  } {
-    domain = "*";
-    type = "hard";
-    item = "nofile";
-    value = "100000";
-  }
-  ];
-
-  virtualisation.docker.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.lulu = {
-     isNormalUser = true;
-     uid = 1000;
-     extraGroups = [ "wheel" "networkmanager" "docker" ];
-     shell = pkgs.fish;
-  };
-
-  # You should change this only after NixOS release notes say you should.
-  system.nixos.stateVersion = "18.03"; # Did you read the comment?
-  # system.stateVersion = "18.03"; # Did you read the comment?
-
+  users.extraUsers.lulu.extraGroups = [ "networkmanager" ];
 }
