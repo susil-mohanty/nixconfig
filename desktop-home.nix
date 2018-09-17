94869af0d29c69f5b046f220049e6e996b2f71ff
sveitser <sveitser@gmail.com>
2018-09-17 21:32:45 +0800

    Add to transmission group

--- desktop-home.nix
+++ desktop-home.nix
@@ -73,6 +73,7 @@
   services.transmission.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.lulu.extraGroups = [ "transmission" ];
   users.extraUsers.dong = {
      isNormalUser = true;
      uid = 1011;
