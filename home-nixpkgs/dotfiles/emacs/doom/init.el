56faf95e58537e66a61d31675c535c8ad8cd147c
Mathis Antony <sveitser@gmail.com>
2018-10-16 21:00:44 +0800

    update packages

--- home-nixpkgs/dotfiles/emacs/doom/init.el
+++ home-nixpkgs/dotfiles/emacs/doom/init.el
@@ -131,7 +131,7 @@
        ;; should be loaded late.
        :app
       ;(email +gmail)    ; emacs as an email client
-      ;irc               ; how neckbeards socialize
+      irc               ; how neckbeards socialize
       ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
       ;(write            ; emacs as a word processor (latex + org + markdown)
