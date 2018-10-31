21c9bb2430872ec36814b2c613cbf7f3c5a85675
Mathis Antony <sveitser@gmail.com>
2018-10-31 16:07:56 +0800

    Keep rtl8821au package in this repo

--- desktop-work.nix
+++ desktop-work.nix
@@ -1,33 +1,21 @@
 { config, pkgs, ... }:
 
 {
-  # Use local nixpkgs checkout
-  # The first time, might have to run:
-  #   nixos-rebuild -I nixpkgs=/etc/nixos/nixpkgs -I nixos-config=/etc/nixos/configuration.nix switch
-  nix.nixPath = [
-    "nixpkgs=/etc/nixos/nixpkgs"
-    "nixos-config=/etc/nixos/configuration.nix"
-  ];
-  nix.autoOptimiseStore = true;
-
   imports =
     [ # Include the results of the hardware scan.
      ./hardware-configuration.nix
      ./pc-common.nix
     ];
 
-  # boot.blacklistedKernelModules = [ "rtl8192se" ];
-  boot.initrd.kernelModules = [ "8812au" ];
-  boot.extraModulePackages = [ pkgs.linuxPackages.rtl8821au ];
+  # USB wifi
+  boot.extraModulePackages = [
+     (pkgs.callPackage ./overlays/rtl8821au { kernel = pkgs.linuxPackages.kernel; })
+  ];
 
   networking.hostName = "mas";
   networking.networkmanager.enable = true;
+  users.extraUsers.lulu.extraGroups = [ "networkmanager" ];
 
   powerManagement.cpuFreqGovernor = "performance";
 
-  # environment.systemPackages = with pkgs; [
-  #   linuxPackages.rtlwifi_new
-  # ];
-
-  users.extraUsers.lulu.extraGroups = [ "networkmanager" ];
 }
