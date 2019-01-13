909c133733a0e48dd449af033630ba650ecb5a63
sveitser <sveitser@gmail.com>
2019-01-13 21:05:38 +0800

    gpg-agent: max ttl 10 hours

--- home-nixpkgs/home.nix
+++ home-nixpkgs/home.nix
@@ -112,7 +112,9 @@ in
   services.gpg-agent = {
     enable = true;
     defaultCacheTtl = 36000;
+    maxCacheTtl = 36000;
     defaultCacheTtlSsh = 36000;
+    maxCacheTtlSsh = 36000;
     enableSshSupport = true;
   };
 
