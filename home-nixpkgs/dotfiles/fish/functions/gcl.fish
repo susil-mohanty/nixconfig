9177fccb0054c55db238ffff7c750e70920f2c67
sveitser <sveitser@gmail.com>
2019-02-23 12:28:14 +0800

    gcl: cd into directory after clone

--- home-nixpkgs/dotfiles/fish/functions/gcl.fish
+++ home-nixpkgs/dotfiles/fish/functions/gcl.fish
@@ -1,5 +1,9 @@
 function gcl
 	set -l repo $argv
+    set -l checkout_dir ~/r/$repo
 	mkdir -p ~/r/(dirname $repo)
-    git clone git@github.com:$repo ~/r/$repo
+    git clone git@github.com:$repo $checkout_dir
+    cd $checkout_dir
+    ls
+    echo Done!
 end
