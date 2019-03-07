b65815dae5882487af592265efac7aae075ac7a0
sveitser <sveitser@gmail.com>
2019-03-07 13:31:58 +0800

    emacs: fix org directory

--- home-nixpkgs/dotfiles/emacs/doom/config.el
+++ home-nixpkgs/dotfiles/emacs/doom/config.el
@@ -1,35 +1,21 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+(setq org-directory "~/org/my")
+(setq org-journal-dir "~/Dropbox/jrn")
+(setq org-journal-date-format "%Y-%m-%d %A")
+(setq org-agenda-files (quote ("~/org/my")))
+(setq evil-escape-key-sequence "hh")
+
+(setq mocha-snippets-use-fat-arrows t)
+
 (add-hook 'js2-mode-hook #'prettier-js-mode)
 (add-hook 'typescript-mode-hook #'prettier-js-mode)
 (add-hook 'python-mode-hook 'blacken-mode)
 
-(setq org-journal-dir "~/Dropbox/jrn")
-(setq org-journal-date-format "%Y-%m-%d %A")
-(setq org-agenda-files (quote ("~/org/my")))
-
-(setq mocha-snippets-use-fat-arrows t)
-;; coulnd't get this to work yet
-;; (with-eval-after-load 'flycheck
-;;  (flycheck-jest-setup))
-
-;; fish creates problems with indium does't seem to have desired effect
-;; (setq-default explicit-shell-file-name "/run/current-system/sw/bin/bash")
-;; (setenv "SHELL" "/run/current-system/sw/bin/bash")
-
 (direnv-mode)
-
-;; (set! :irc "chat.freenode.net"
-;;     `(:tls t
-;;       :nick "sveitser"
-;;       :port 6697
-;;       :sasl-username ,(+pass-get-user "freenode.net/sveitser@gmail.com")
-;;       :sasl-password (lambda (&rest _) (+pass-get-secret "freenode.net/sveitser@gmail.com"))
-;;       :channels ("#nixos")))
-
-
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
 (load-theme 'doom-dracula t)
 
@@ -39,18 +25,6 @@
 
 ;; Corrects (and improves) org-mode's native fontification.
 (doom-themes-org-config)
-
-(setq mocha-which-node "node")
-(load! "local/jest")
-(after! mocha
-  (set-popup-rule! "^\\*mocha tests*"
-                   :side 'right
-                   :size 140
-                   :select nil
-                   :quit nil
-                   :ttl t))
-
-(setq evil-escape-key-sequence "hh")
 
 (require 'lsp)
 (require 'lsp-clients)
@@ -72,3 +46,4 @@
 
 (add-hook 'after-save-hook 'magit-after-save-refresh-status)
 (require 'org-protocol)
+
